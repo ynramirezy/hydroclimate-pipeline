6397f148167735121721d4e0147c4a1899d2893e
Yessica Yara <ynramirezy@unal.edu.co>
2026-02-26 15:36:13 +0100

    Updating file for linux compatibility

--- pipeline/global.R
+++ pipeline/global.R
@@ -7,10 +7,8 @@ if (!renv::status()$synchronized) {
 }
 
 #Loading libraries
-required_packages <- c("automap", "climate", "data.table", "dplyr", "FNN", 
-  "gstat", "httr", "lubridate", "raster", "sp", "terra", "whitebox")
-
-#invisible(lapply(required_packages, library, character.only = TRUE))
+required_packages <- c("automap", "climate", "data.table", "dplyr", "FNN", "gstat", "httr", "lubridate", "raster", "sp", "terra", "whitebox")
+invisible(lapply(required_packages, library, character.only = TRUE))
 if (!whitebox::wbt_init()) {
   message("Whitebox engine not found. Installing...")
   whitebox::wbt_install()
