575f82ab2faf22b46d4ce5604bae86fa7fd597b0
Yessica Yara <ynramirezy@unal.edu.co>
2026-02-27 00:37:50 +0100

    Setup pipeline artifacts for CD outputs

--- pipeline/functions/validation_runoff.R
+++ pipeline/functions/validation_runoff.R
@@ -4,26 +4,27 @@ validation_runoff<- function() {
   runoff_gee_stack <- rast(list.files(path = file.path(outputs[["Validation_output"]]), pattern = "\\.tif$", full.names = TRUE))
   datesr= dates[(n_days+1):length(dates)]
   results <- data.frame()
-  col_to_use <- function(runoff_col, item) {
-    if(item==1){
-      u <- unique(runoff_col)
-      if(all(u == 1)) {
-        "darkgreen"
-      } else if (all(u == 0)){
-        "lightgray"
-      } else {
-        c("lightgray", "darkgreen")
-      }
+  colors_gee <- c("lightgray", "darkgreen")
+  colors_ral <- c("red", "green")
+  col_to_use <- function(runoff_ras, colors_map) {
+    u <- unique(runoff_ras)[, 1]
+    if(all(u == 0)) {
+      colors_map[1]
+    } else if (all(u == 1)){
+      colors_map[2]
     } else {
-      u <- unique(runoff_col)[, 1]
-      if(all(u == 1)) {
-        "green"
-      } else if (all(u == 0)){
-        "red"
-      } else {
-      c("red", "green")
-      }    
+      colors_map
     }
+  }
+  validation_plot <- function(gee_raster, ral_raster, gee_vect){
+    old_mar <- par()$mar
+    par(mar = c(3, 2, 2, 2)) 
+    plot(gee_raster, col = col_to_use(gee_raster, colors_gee), legend = FALSE,  main= paste("Runoff Presence Comparison ERA5 vs Pipeline", datesr[i]), mar = par("mar"))
+    plot(gee_vect, add = TRUE, border = "grey70", col = "transparent", legend = FALSE)
+    image(ral_raster, col = adjustcolor(col_to_use(ral_raster, colors_ral), alpha.f = 0.6), add = TRUE, legend = FALSE)
+    plot(vect(file.path(outputs[["GIS_data_output"]], paste0("Powiat_", polygon_name, ".shp"))), add = TRUE, legend = FALSE)
+    legend("bottom", inset = c(0, -0.1), legend = c("ERA-5 present", "ERA-5 absent", "Pipeline present", "Pipeline absent"), fill = c("darkgreen", "lightgray", "green", "red"), border = "black", bty = "n", xpd = TRUE, ncol = 4, cex = 0.7)
+    par(mar = old_mar)
   }
   for (i in 1:nlyr(run_stack)) {
     # Binary runoff
@@ -37,17 +38,12 @@ validation_runoff<- function() {
     runoff_ratio <- na.omit(cbind(values(gee_poly)[[1]], runoff_zonal[[1]], runoff_edges[[1]]))
     results <- rbind(results, validation_statistics(runoff_ratio[,1], runoff_ratio[,2] / runoff_ratio[,3], format(datesr[i], "%Y%m%d")))
     # Ploting
-    old_mar <- par()$mar
-    par(mar = c(3, 2, 2, 2)) 
-    plot(gee_bin, col = col_to_use(runoff_ratio[,1], 1), legend = FALSE,  main= paste("Runoff Presence Comparison ERA5 vs Pipeline", datesr[i]), mar = par("mar"))
-    plot(gee_poly, add = TRUE, border = "grey70", col = "transparent")
-    plot(ral_bin, col = adjustcolor(col_to_use(ral_bin, 2), alpha.f = 0.6), add = TRUE, legend = FALSE)
-    plot(vect(file.path(outputs[["GIS_data_output"]], paste0("Powiat_", polygon_name, ".shp"))), add = TRUE)
-    legend("bottom", inset = c(0, -0.1), legend = c("ERA-5 present", "ERA-5 absent", "Pipeline present", "Pipeline absent"), fill = c("darkgreen", "lightgray", "green", "red"), border = "black", bty = "n", xpd = TRUE, ncol = 4, cex = 0.7)
-    par(mar = old_mar)
-    dev.copy(png, filename = file.path(outputs[["Validation_output"]], paste0("Validation_Runoff_", format(datesr[i], "%Y%m%d"), ".png")), width = 3400, height = 2000, res = 300 )
+    validation_plot(gee_bin, ral_bin, gee_poly)
+    agg_png(filename = file.path(outputs[["Validation_output"]], paste0("Validation_Runoff_", format(datesr[i], "%Y%m%d"), ".png")), width = 3400, height = 2000, res = 300)
+      validation_plot(gee_bin, ral_bin, gee_poly)
     dev.off()   
   }
+  cat(paste("\n"))
   print(results)
   write.csv(results, file.path(outputs[["Validation_output"]], "Validacion_runoff_statistics.csv"), row.names = FALSE)
   cat(paste0("\n\n🎯 The overall accuracy of the Pipeline Runoff presence is: ", + mean(results$Accuracy)*100, "%"))  
--- pipeline/global.R
+++ pipeline/global.R
@@ -7,7 +7,7 @@ if (!renv::status()$synchronized) {
 }
 
 #Loading libraries
-required_packages <- c("automap", "climate", "data.table", "dplyr", "FNN", "gstat", "httr", "lubridate", "raster", "sp", "terra", "whitebox")
+required_packages <- c("automap", "climate", "data.table", "dplyr", "FNN", "gstat", "httr", "lubridate", "ragg", "raster", "sp", "terra", "whitebox")
 invisible(lapply(required_packages, library, character.only = TRUE))
 if (!whitebox::wbt_init()) {
   message("Whitebox engine not found. Installing...")
